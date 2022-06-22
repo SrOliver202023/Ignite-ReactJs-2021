346a7814cbf24dba76c2c0489722c29375b369ea
SrOliver202023 <sroliver202023@gmail.com>
2022-06-22 14:45:13 -0300

    :sparkles: Chapter 1 | Contextos e hooks | A Context API no React

--- Chapter 2/dtmoney/src/components/Summary/index.tsx
+++ Chapter 2/dtmoney/src/components/Summary/index.tsx
@@ -1,10 +1,15 @@
-import IncomeSvg from '../../assets/income.svg';
-import OutcomeSvg from '../../assets/outcome.svg';
-import TotalSvg from '../../assets/total.svg';
+import { useContext } from "react";
+import IncomeSvg from "../../assets/income.svg";
+import OutcomeSvg from "../../assets/outcome.svg";
+import TotalSvg from "../../assets/total.svg";
+import { Transactions } from "../../contexts/Transactions";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export function Summary() {
+  const transactions = useContext(Transactions);
+  console.log(transactions);
+
   return (
     <Container>
       <div>
@@ -21,15 +26,13 @@ export function Summary() {
         </header>
         <strong>- R$1000,00</strong>
       </div>
-      <div className='highlight-background'>
+      <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={TotalSvg} alt="Total" />
         </header>
         <strong>R$1000,00</strong>
       </div>
-
-
     </Container>
   );
 }
