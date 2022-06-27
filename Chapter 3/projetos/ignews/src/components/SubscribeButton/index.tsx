588772885cb33e42fcc5b69ac624ae4916a4b675
SrOliver202023 <sroliver202023@gmail.com>
2022-06-27 05:34:05 -0300

    :sparkles: Chapter 3 | Pagamentos no Stripe | Gerando sessão de checkout

--- Chapter 3/projetos/ignews/src/components/SubscribeButton/index.tsx
+++ Chapter 3/projetos/ignews/src/components/SubscribeButton/index.tsx
@@ -1,12 +1,33 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
 	priceId: String;
 }
 
+// Security Locales
+// getServerProps (SSR)
+// getStaticProps (SSG)
+// API Routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+	const session = useSession();
+
+	function handleSubscribeButton() {
+		if (session.status !== "authenticated") {
+			signIn("github");
+			return;
+		}
+
+		// Checkout Session
+	}
+
 	return (
-		<button type="button" className={styles.subscribeButton}>
+		<button
+			type="button"
+			className={styles.subscribeButton}
+			onClick={handleSubscribeButton}
+		>
 			Subscribe now
 		</button>
 	);
