778ccab39907da9c023c0f9397cc8014ff0df362
SrOliver202023 <sroliver202023@gmail.com>
2022-06-28 05:01:32 -0300

    :sparkles: Chapter 3 | Pagamentos no Stripe | Evitando duplicação no Stripe

--- Chapter 3/projetos/ignews/src/services/stripe.ts
+++ Chapter 3/projetos/ignews/src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 import axios from "axios";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+export const stripe = new Stripe(`${process.env.STRIPE_API_KEY}`, {
 	apiVersion: "2020-08-27",
 	appInfo: {
 		name: "Ignews",
